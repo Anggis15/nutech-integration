206f92ef8a0a168dfbc5246b98731cd928a1ebb4
Anggi Saputra <asd@asds-MacBook-Pro.local>
2024-12-06 17:25:17 +0700

    add Password

--- src/main/java/com/apply/applyKerja/security/Crypto.java
+++ src/main/java/com/apply/applyKerja/security/Crypto.java
@@ -56,7 +56,7 @@ public class Crypto {
         String encryptionResult = new Crypto().performEncrypt(strToEncrypt);
         System.out.println("Encryption Result : "+encryptionResult);
 
-        String strToDecrypt = "abb07b3e6ce49452eee7f5532759ade2d2406978a603f395851497fce43e13be4790241bc369884170b131c6504a643e1b290a65b91f2f4acf0d197f0a607c708678ed451d1bc430011f34e32b1ff35d57120a81bd958fb8ab2ad05ab858722862343f4a564719cc2aea2ec673b6bc3f";//put text to decrypt in here
+        String strToDecrypt = "90e782a64efb3954bf02f3d4b42b5b80";//put text to decrypt in here
         String decriptionResult = new Crypto().performDecrypt(strToDecrypt);
         System.out.println("Decryption Result : "+decriptionResult);
     }
